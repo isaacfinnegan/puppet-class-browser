d720a8b4ce836b082e6fe95666da62528b4b7ba6
Isaac Finnegan <ifinnegan@evernote.com>
2014-06-18 09:40:43 -0700

    removing now unused files

--- update_class_list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for FILE in modules/*/manifests/*.pp
-do
-    export FILE
-    egrep "^(class)" $FILE | awk '{  print $2 "---" ENVIRON["FILE"]}' >> classes.unsorted
-done
-cat classes.unsorted|sort > classes
-rm classes.unsorted
